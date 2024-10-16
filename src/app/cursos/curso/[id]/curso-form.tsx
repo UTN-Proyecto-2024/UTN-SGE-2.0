698989b47db11f98b048f4f34ca33559aa29c94a
Alexander Armua <alexanderarmua1@gmail.com>
2024-10-16 11:30:29 -0300

    Cambios 01

--- src/app/cursos/curso/[id]/curso-form.tsx
+++ src/app/cursos/curso/[id]/curso-form.tsx
@@ -1,8 +1,6 @@
-"use client";
-
 import { FormProvider, useForm } from "react-hook-form";
 import { api } from "@/trpc/react";
-import { Button, toast } from "@/components/ui";
+import { Button, ScrollArea, toast } from "@/components/ui";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { type z } from "zod";
 import { useEffect, useMemo, useState } from "react";
@@ -103,7 +101,7 @@ export const CursoForm = ({ id, onSubmit, onCancel }: Props) => {
       dia2: curso?.dia2 ?? undefined,
       profesorUserId: curso?.profesorId ?? "",
       ayudanteUsersIds: curso?.ayudantes?.map((a) => a.usuario.id) ?? [],
-      anioDeCarrera: curso?.anioDeCarrera ?? undefined,
+      anioDeCarrera: curso?.anioDeCarrera ? String(curso?.anioDeCarrera) : "",
       activo: curso?.activo ?? true,
       ac: curso?.ac ?? "",
       sedeId: curso?.sedeId?.toString() ?? "",
@@ -124,7 +122,6 @@ export const CursoForm = ({ id, onSubmit, onCancel }: Props) => {
 
   console.log(formHook.formState.errors);
 
-  // TODO: Separar componente de formulario y logica de carga y actualización de curso
   useEffect(() => {
     if (curso) {
       formHook.reset({
@@ -137,7 +134,7 @@ export const CursoForm = ({ id, onSubmit, onCancel }: Props) => {
         dia2: curso.dia2 ?? undefined,
         profesorUserId: curso.profesorId,
         ayudanteUsersIds: curso.ayudantes?.map((a) => a.usuario.id) ?? [],
-        anioDeCarrera: curso.anioDeCarrera,
+        anioDeCarrera: curso.anioDeCarrera ? String(curso.anioDeCarrera) : "",
         activo: curso.activo,
         ac: curso.ac,
         sedeId: curso.sedeId?.toString(),
@@ -196,124 +193,118 @@ export const CursoForm = ({ id, onSubmit, onCancel }: Props) => {
   return (
     <FormProvider {...formHook}>
       <form onSubmit={handleSubmit(onFormSubmit)} className="relative flex w-full flex-col gap-4">
-        <div className="flex w-full flex-col items-center justify-center">
-          <div className="flex flex-col space-y-4 px-0 md:px-6">
-            <div className="flex w-full flex-row lg:flex-row lg:justify-between lg:gap-x-4">
-              <div className="mt-4 w-full md:basis-2/3">
-                <SelectMateriasForm
-                  label={"Materia"}
-                  control={control}
-                  name="materiaId"
-                  placeholder={"Seleccione una materia"}
-                />
-              </div>
-              <div className="flex w-full flex-row gap-x-4 lg:flex-row lg:justify-between">
-                <div className="mt-4 w-full md:basis-2/3">
+        <ScrollArea className="max-h-[calc(100vh_-_20%)] w-full pr-4 md:max-h-[calc(100vh_-_30%)] lg:max-h-[calc(100vh_-_30%)]">
+          <div className="flex w-full flex-col items-center justify-center">
+            <div className="flex w-full flex-col space-y-4 px-0 md:px-6">
+              <div className="flex w-full flex-row gap-x-4">
+                <div className="basis-1/2">
+                  <SelectMateriasForm
+                    label={"Materia"}
+                    control={control}
+                    name="materiaId"
+                    placeholder={"Seleccione una materia"}
+                  />
+                </div>
+                <div className="basis-1/2">
                   <SelectSedeForm label={"Sede"} control={control} name="sedeId" placeholder={"Seleccione una sede"} />
                 </div>
               </div>
-            </div>
-            <div className="flex w-full flex-row gap-x-4 lg:flex-row lg:justify-between">
-              <div className="mt-4 w-full md:basis-2/3">
-                <FormSelect label={"Duración"} control={control} name="ac" className="mt-2" items={ac} />
+
+              <div className="flex w-full flex-row gap-x-4">
+                <div className="mt-4 w-full">
+                  <FormSelect label={"Duración"} control={control} name="ac" className="mt-2" items={ac} />
+                </div>
+
+                <div className="mt-4 w-full">
+                  <FormSelect label={"Turno"} control={control} name="turno" className="mt-2" items={turnosValues} />
+                </div>
+
+                <div className="mt-4 w-full">
+                  <FormSelect
+                    label={"Año"}
+                    control={control}
+                    name="anioDeCarrera"
+                    className="mt-2"
+                    items={aniosDeCarrera}
+                  />
+                </div>
+                <div className="mt-4 w-full">
+                  <SelectDivisionesForm
+                    label={"División"}
+                    control={control}
+                    name="divisionId"
+                    placeholder={"Seleccione una división"}
+                    className="mt-2"
+                  />
+                </div>
               </div>
 
-              <div className="mt-4 w-full md:basis-2/3">
-                <FormSelect label={"Turno"} control={control} name="turno" className="mt-2" items={turnosValues} />
+              <div className="flex w-full flex-row gap-x-4 lg:flex-row lg:justify-between">
+                <div className="mt-4 basis-1/3">
+                  <FormSelect label={"Día 1"} control={control} name="dia1" className="mt-2" items={dias} />
+                </div>
+
+                <div className="mt-4 basis-1/3">
+                  <FormSelect
+                    label={"Hora inicio 1"}
+                    control={control}
+                    name="horaInicio1"
+                    className="mt-2"
+                    items={horas}
+                  />
+                </div>
+
+                <div className="mt-4 basis-1/3">
+                  <FormSelect
+                    label={"Duración 1"}
+                    control={control}
+                    name="duracion1"
+                    items={duracion}
+                    className="mt-2"
+                  />
+                </div>
               </div>
 
-              <div className="mt-4 w-full md:basis-2/3">
-                <select
-                  {...formHook.register("anioDeCarrera", {
-                    required: "Debes seleccionar un año",
-                    valueAsNumber: true,
-                    validate: (value) =>
-                      typeof value === "number" && value >= 1 && value <= 6 ? true : "Selecciona un año válido",
-                  })}
-                  className="mt-2 block w-full rounded-md border-gray-300 bg-white text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white"
-                >
-                  <option value="">Seleccionar año</option>
-                  <option value={1}>1</option>
-                  <option value={2}>2</option>
-                  <option value={3}>3</option>
-                  <option value={4}>4</option>
-                  <option value={5}>5</option>
-                  <option value={6}>6</option>
-                </select>
-              </div>
-              <div className="mt-4 w-full md:basis-1/3">
-                <SelectDivisionesForm
-                  label={"División"}
-                  control={control}
-                  name="divisionId"
-                  placeholder={"Seleccione una división"}
-                />
+              <div className="flex w-full flex-row gap-x-4 lg:flex-row lg:justify-between">
+                {mostrarDia2 ? (
+                  <DiaAdicionalForm
+                    control={control}
+                    dias={dias}
+                    horas={horas}
+                    duracion={duracion}
+                    onRemove={handleRemoveDia2}
+                  />
+                ) : (
+                  <button type="button" className="mt-4 text-blue-600" onClick={handleAddDia2}>
+                    + Agregar Día 2
+                  </button>
+                )}
               </div>
 
-              <div className="mt-4 basis-1/3">
-                {/* <FormSelect label={"Sede"} control={control} name="sedeId" className="mt-2" items={sedes} /> */}
-              </div>
-            </div>
+              <div className="flex w-full flex-row gap-x-4 lg:flex-row lg:justify-between">
+                <div className="mt-4 basis-1/2">
+                  <FormSelect
+                    label={"Profesor"}
+                    control={control}
+                    name="profesorUserId"
+                    className="mt-2"
+                    items={profesores}
+                  />
+                </div>
 
-            <div className="flex w-full flex-row gap-x-4 lg:flex-row lg:justify-between">
-              <div className="mt-4 basis-1/3">
-                <FormSelect label={"Día 1"} control={control} name="dia1" className="mt-2" items={dias} />
-              </div>
-
-              <div className="mt-4 basis-1/3">
-                <FormSelect
-                  label={"Hora inicio 1"}
-                  control={control}
-                  name="horaInicio1"
-                  className="mt-2"
-                  items={horas}
-                />
-              </div>
-
-              <div className="mt-4 basis-1/3">
-                <FormSelect label={"Duración 1"} control={control} name="duracion1" items={duracion} className="mt-2" />
-              </div>
-            </div>
-
-            <div className="flex w-full flex-row gap-x-4 lg:flex-row lg:justify-between">
-              {mostrarDia2 ? (
-                <DiaAdicionalForm
-                  control={control}
-                  dias={dias}
-                  horas={horas}
-                  duracion={duracion}
-                  onRemove={handleRemoveDia2}
-                />
-              ) : (
-                <button type="button" className="mt-4 text-blue-600" onClick={handleAddDia2}>
-                  + Agregar Día 2
-                </button>
-              )}
-            </div>
-
-            <div className="flex w-full flex-row gap-x-4 lg:flex-row lg:justify-between">
-              <div className="mt-4 basis-1/2">
-                <FormSelect
-                  label={"Profesor"}
-                  control={control}
-                  name="profesorUserId"
-                  className="mt-2"
-                  items={profesores}
-                />
-              </div>
-
-              <div className="mt-4 basis-1/2">
-                <FormSelect
-                  label={"Ayudante"}
-                  control={control}
-                  name="ayudanteUsersIds"
-                  className="mt-2"
-                  items={ayudantes}
-                />
+                <div className="mt-4 basis-1/2">
+                  <FormSelect
+                    label={"Ayudante"}
+                    control={control}
+                    name="ayudanteUsersIds"
+                    className="mt-2"
+                    items={ayudantes}
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </ScrollArea>
         <div className="flex w-full flex-row items-end justify-end space-x-4">
           <Button title="Cancelar" type="button" variant="default" color="secondary" onClick={handleCancel}>
             Cancelar
