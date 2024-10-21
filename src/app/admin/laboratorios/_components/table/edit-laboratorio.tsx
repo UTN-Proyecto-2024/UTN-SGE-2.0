aee65653d161e0a4c24ca260888e494d84a571b6
Agustina Razanov <agustinarazanov@gmail.com>
2024-10-21 03:29:20 -0300

    normalize buttons

--- src/app/admin/laboratorios/_components/table/edit-laboratorio.tsx
+++ src/app/admin/laboratorios/_components/table/edit-laboratorio.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { ADMIN_ROUTE } from "@/shared/server-routes";
 import { EditIcon } from "lucide-react";
 import Link from "next/link";
@@ -11,7 +12,9 @@ const rutaAdmin = ADMIN_ROUTE;
 export const EditarLaboratorioModal = ({ laboratorioId }: EditarLaboratorioModalProps) => {
   return (
     <Link key={laboratorioId} href={`${rutaAdmin.href}/laboratorios/${laboratorioId}`} passHref prefetch={false}>
-      <EditIcon />
+      <Button color={"outline"} className="h-8 w-8 px-1 py-1">
+        <EditIcon size={16} />
+      </Button>
     </Link>
   );
 };
