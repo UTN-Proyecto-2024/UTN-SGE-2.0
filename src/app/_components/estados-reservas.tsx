4a1569d4096f8102783f57517040b593efb5a1c7
Alexander Armua <alexanderarmua1@gmail.com>
2024-11-02 04:09:38 -0300

    Cambiado reserva finalizada por pasadas solo texto

--- src/app/_components/estados-reservas.tsx
+++ src/app/_components/estados-reservas.tsx
@@ -2,6 +2,29 @@ import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { cn } from "@/components/utils";
 import { ReservaEstatus } from "@prisma/client";
 
+const reservaEstatus = [
+  {
+    id: ReservaEstatus.PENDIENTE,
+    label: "Pendientes",
+  },
+  {
+    id: ReservaEstatus.FINALIZADA,
+    label: "Pasadas",
+  },
+  {
+    id: ReservaEstatus.CANCELADA,
+    label: "Canceladas",
+  },
+  {
+    id: ReservaEstatus.RECHAZADA,
+    label: "Rechazadas",
+  },
+  {
+    id: "",
+    label: "Todas",
+  },
+];
+
 export const EstadoReservaToString = ({
   handleEstadoChange,
   currentEstado,
@@ -11,17 +34,17 @@ export const EstadoReservaToString = ({
 }) => {
   return (
     <ToggleGroup type="single" className="flex flex-row justify-between overflow-auto rounded-lg bg-primary/20 p-1">
-      {[...Object.values(ReservaEstatus), ""].map((value) => (
+      {reservaEstatus.map(({ id: value, label }) => (
         <ToggleGroupItem
           key={value || "all"}
           value={value}
-          aria-label={`Cambiar a ${value.toLowerCase() || "todas"}`}
+          aria-label={`Cambiar a ${label}`}
           className={cn("h-9 w-full text-center hover:bg-transparent", {
             "bg-primary/50 hover:bg-primary/50": currentEstado === value,
           })}
           onClick={() => handleEstadoChange(value as ReservaEstatus)}
         >
-          {value.length > 0 ? value[0] + value.slice(1).toLowerCase() : "Todas"}
+          {label}
         </ToggleGroupItem>
       ))}
     </ToggleGroup>
