0c49cdea28745e72522b80cf6c0960b5d0f2499f
santiagocast <santiagojcastelli@gmail.com>
2024-11-15 02:19:19 -0300

    [bugs] - permisos - TODO a partir de equipos

--- src/app/biblioteca/layout.tsx
+++ src/app/biblioteca/layout.tsx
@@ -3,6 +3,7 @@
 import { redirect } from "next/navigation";
 import { INICIO_ROUTE } from "@/shared/server-routes";
 import { estaLogueadoYConPermiso } from "@/server/permisos";
+import { SgeNombre } from "@prisma/client";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -10,11 +11,18 @@ type LayoutProps = {
 };
 
 export default async function RootLayout({ children, modal }: LayoutProps) {
-  const puedeEstarAca = await estaLogueadoYConPermiso([]);
-
-  if (!puedeEstarAca) {
+  const puedeVer = await estaLogueadoYConPermiso([
+    SgeNombre.BIBLIOTECA_VER_LISTADO,
+    SgeNombre.BIBLIOTECA_VER_DETALLES_LIBRO,
+    SgeNombre.BIBLIOTECA_BUSCAR_LIBRO,
+    SgeNombre.BIBLIOTECA_ABM_LIBRO,
+    SgeNombre.BIBLIOTECA_PRESTAMO_PRESTAR,
+    SgeNombre.BIBLIOTECA_PRESTAMO_VER_PRESTAMOS,
+    SgeNombre.BIBLIOTECA_EDITORIALES_ABM,
+    SgeNombre.BIBLIOTECA_PRESTAMO_VER_MIS_PRESTAMOS,
+  ]);
+  if (!puedeVer) {
     redirect(INICIO_ROUTE.href);
-    return null;
   }
 
   return (
--- src/app/biblioteca/libros/layout.tsx
+++ src/app/biblioteca/libros/layout.tsx
@@ -1,10 +1,24 @@
 "use server";
 
+import { estaLogueadoYConPermiso } from "@/server/permisos";
+import { INICIO_ROUTE } from "@/shared/server-routes";
+import { SgeNombre } from "@prisma/client";
+import { redirect } from "next/navigation";
+
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 export default async function RootLayout({ children }: LayoutProps) {
+  const puedeVer = await estaLogueadoYConPermiso([
+    SgeNombre.BIBLIOTECA_ABM_LIBRO,
+    SgeNombre.BIBLIOTECA_VER_DETALLES_LIBRO,
+    SgeNombre.BIBLIOTECA_VER_LISTADO,
+    SgeNombre.BIBLIOTECA_BUSCAR_LIBRO,
+  ]);
+  if (!puedeVer) {
+    redirect(INICIO_ROUTE.href);
+  }
   return (
     <main className="flex flex-col">
       <div className="flex flex-col items-center justify-center gap-12 px-4 py-16 ">{children}</div>
