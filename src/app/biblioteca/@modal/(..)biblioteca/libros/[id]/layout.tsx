d7819e38432f4f35b656de0777791b01294b60a3
santiagocast <santiagojcastelli@gmail.com>
2024-11-15 23:51:54 -0300

    [permisos] - fix layout builder

--- src/app/biblioteca/@modal/(..)biblioteca/libros/[id]/layout.tsx
+++ src/app/biblioteca/@modal/(..)biblioteca/libros/[id]/layout.tsx
@@ -5,18 +5,12 @@ import { redirect } from "next/navigation";
 
 type LayoutProps = {
   children: React.ReactNode;
-  modal: React.ReactNode;
 };
 
-export default async function Layout({ children, modal }: LayoutProps) {
+export default async function Layout({ children }: LayoutProps) {
   const puedeVer = await estaLogueadoYConPermiso([SgeNombre.BIBLIOTECA_ABM_LIBRO]);
   if (!puedeVer) {
     redirect(INICIO_ROUTE.href);
   }
-  return (
-    <div className="flex w-full flex-col">
-      {children}
-      {modal}
-    </div>
-  );
+  return <div className="flex w-full flex-col">{children}</div>;
 }
--- src/app/laboratorios/mis_cursos/@modal/(...)laboratorios/mis_cursos/[id]/layout.tsx
+++ src/app/laboratorios/mis_cursos/@modal/(...)laboratorios/mis_cursos/[id]/layout.tsx
@@ -7,19 +7,13 @@ import { SgeNombre } from "@prisma/client";
 
 type LayoutProps = {
   children: React.ReactNode;
-  modal: React.ReactNode;
 };
 
-export default async function RootLayout({ children, modal }: LayoutProps) {
+export default async function RootLayout({ children }: LayoutProps) {
   const puedeVer = await estaLogueadoYConPermiso([SgeNombre.RES_LAB_RESERVAR_CURSO_AUTO]);
   if (!puedeVer) {
     redirect(INICIO_ROUTE.href);
   }
 
-  return (
-    <main className="flex flex-col">
-      {children}
-      {modal}
-    </main>
-  );
+  return <main className="flex flex-col">{children}</main>;
 }
