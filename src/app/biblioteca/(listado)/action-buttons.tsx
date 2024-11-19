d0def00da72166cfeb488e75121c5266eb7a1891
comejia <comejia@teco.com.ar>
2024-11-19 13:57:32 -0300

    Permisos en Biblioteca

--- src/app/biblioteca/(listado)/action-buttons.tsx
+++ src/app/biblioteca/(listado)/action-buttons.tsx
@@ -3,6 +3,8 @@ import { BibliotecaFilterMateria } from "./filtros/biblioteca-filter-materia";
 import { BibliotecaFilterText } from "./filtros/biblioteca-filter-text";
 
 import { type inputGetBooks } from "@/shared/filters/biblioteca-filter.schema";
+import { TienePermiso } from "@/app/_components/permisos/tienePermiso";
+import { SgeNombre } from "@prisma/client";
 
 type BibliotecaFilters = z.infer<typeof inputGetBooks>;
 
@@ -15,10 +17,14 @@ export const ActionButtons = ({ filters }: ActionButtonsProps) => {
     <div className="relative flex w-full flex-col items-center justify-between space-y-3 md:flex-row md:space-x-1.5 md:space-y-0">
       <div className="w-full space-y-3 sm:flex sm:flex-row sm:space-x-3 sm:space-y-0 md:basis-1/2">
         <div className="md:basis-1/2">
-          <BibliotecaFilterText filters={filters} />
+          <TienePermiso permisos={[SgeNombre.BIBLIOTECA_BUSCAR_LIBRO]}>
+            <BibliotecaFilterText filters={filters} />
+          </TienePermiso>
         </div>
         <div className="md:basis-1/2">
-          <BibliotecaFilterMateria filters={filters} />
+          <TienePermiso permisos={[SgeNombre.BIBLIOTECA_BUSCAR_LIBRO]}>
+            <BibliotecaFilterMateria filters={filters} />
+          </TienePermiso>
         </div>
       </div>
     </div>
--- src/app/biblioteca/page.tsx
+++ src/app/biblioteca/page.tsx
@@ -7,6 +7,8 @@ import LoadingBibliotecaTable from "./(listado)/loading-biblioteca-table";
 import PageLayout from "@/components/ui/page-template";
 import { BIBLIOTECA_ROUTE } from "@/shared/server-routes";
 import { BibliotecaNewLibro } from "./(listado)/biblioteca-new-book";
+import { TienePermiso } from "../_components/permisos/tienePermiso";
+import { SgeNombre } from "@prisma/client";
 
 type PageProps = {
   searchParams: ReadonlyURLSearchParams;
@@ -18,7 +20,14 @@ export default function Page({ searchParams }: PageProps) {
   const filter_as_key = useMemo(() => JSON.stringify(filters), [filters]);
 
   return (
-    <PageLayout route={BIBLIOTECA_ROUTE} buttons={<BibliotecaNewLibro />}>
+    <PageLayout
+      route={BIBLIOTECA_ROUTE}
+      buttons={
+        <TienePermiso permisos={[SgeNombre.BIBLIOTECA_ABM_LIBRO]}>
+          <BibliotecaNewLibro />
+        </TienePermiso>
+      }
+    >
       <ActionButtons filters={filters} />
       <Suspense key={filter_as_key} fallback={<LoadingBibliotecaTable />}>
         <BibliotecaTableContainer filters={filters} />
