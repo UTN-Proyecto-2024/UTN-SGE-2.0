f68d360816a064807abc389eb559308a10329558
comejia <comejia@teco.com.ar>
2024-11-19 16:56:44 -0300

    Permisos de Labo cerrado

--- src/app/laboratorios/_components/table/table.tsx
+++ src/app/laboratorios/_components/table/table.tsx
@@ -4,6 +4,8 @@ import { DataTable } from "@/components/ui";
 import { type RouterOutputs } from "@/trpc/react";
 import { ReservarLaboratorioCerradoModal } from "./reservar-laboratorio";
 import { getColumns } from "./columns";
+import { TienePermiso } from "@/app/_components/permisos/tienePermiso";
+import { SgeNombre } from "@prisma/client";
 // import { TienePermiso } from "@/app/_components/permisos/tienePermiso";
 // import { SgeNombre } from "@prisma/client";
 
@@ -40,7 +42,12 @@ export const CursosTable = ({ data }: CursosTableProps) => {
           cell({ original }) {
             return (
               <>
-                <ReservarLaboratorioCerradoModal cursoId={original.id} />
+                {/* TOOD: esta table se llama desde "Catedra" y "Mis Cursos". Puede haber conflictos al poner estos dos permisos?? */}
+                <TienePermiso
+                  permisos={[SgeNombre.RES_LAB_RESERVAR_CURSO_AUTO, SgeNombre.RES_LAB_RESERVAR_CATEDRA_AUTO]}
+                >
+                  <ReservarLaboratorioCerradoModal cursoId={original.id} />
+                </TienePermiso>
               </>
             );
           },
