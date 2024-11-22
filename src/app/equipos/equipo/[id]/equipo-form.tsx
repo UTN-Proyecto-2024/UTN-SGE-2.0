25d22ed57ed6e1d521b3a4eef9726961926894d6
Alex <alexanderarmua1@gmail.com>
2024-11-22 10:46:28 -0300

    fixes generales

--- src/app/equipos/equipo/[id]/equipo-form.tsx
+++ src/app/equipos/equipo/[id]/equipo-form.tsx
@@ -1,6 +1,6 @@
 import { FormProvider, useForm } from "react-hook-form";
 import { api } from "@/trpc/react";
-import { Button, FormInput, ScrollArea, toast } from "@/components/ui";
+import { Button, FormInput, toast } from "@/components/ui";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { type z } from "zod";
 import { useEffect, useMemo } from "react";
@@ -135,160 +135,158 @@ export const EquipoForm = ({ id, onSubmit, onCancel }: Props) => {
   return (
     <FormProvider {...formHook}>
       <form onSubmit={handleSubmit(onFormSubmit)} className="relative flex w-full flex-col gap-4">
-        <ScrollArea className="max-h-[calc(100vh_-_300px)] w-full pr-4">
-          <div className="flex w-full flex-col justify-center lg:items-center">
-            <div className="flex flex-col space-y-4 px-0 md:px-6">
-              <div className="flex w-full flex-col gap-x-4 md:flex-row lg:flex-row lg:justify-between">
-                <div className="mt-4 basis-1/3">
-                  <FormInput
-                    label={"Inventario"}
-                    control={control}
-                    name="inventarioId"
-                    placeholder={"Ingrese el inventario"}
-                    type={"text"}
-                    className="mt-2"
-                  />
-                </div>
-
-                <div className="mt-4 basis-1/3">
-                  <SelectMarcasForm
-                    name="marca"
-                    realNameId="marcaId"
-                    control={control}
-                    className="mt-2"
-                    label={"Marca"}
-                    placeholder={"Seleccione una marca"}
-                  />
-                </div>
-
-                <div className="mt-4 basis-1/3">
-                  <SelectModelosForm
-                    label={"Modelo"}
-                    control={control}
-                    name="modeloForm"
-                    realNameId="modelo"
-                    className="mt-2"
-                    placeholder={"Seleccione un modelo"}
-                  />
-                </div>
+        <div className="flex w-full flex-col justify-center lg:items-center">
+          <div className="flex flex-col space-y-4 px-0 md:px-6">
+            <div className="flex w-full flex-col gap-x-4 md:flex-row lg:flex-row lg:justify-between">
+              <div className="mt-4 basis-1/3">
+                <FormInput
+                  label={"Inventario"}
+                  control={control}
+                  name="inventarioId"
+                  placeholder={"Ingrese el inventario"}
+                  type={"text"}
+                  className="mt-2"
+                />
               </div>
 
-              <div className="flex w-full flex-col gap-x-4 md:flex-row lg:flex-row lg:justify-between">
-                <div className="mt-4 basis-1/2">
-                  <FormInput
-                    label={"Número de serie"}
-                    control={control}
-                    name="numeroSerie"
-                    placeholder={"Ingrese el número de serie"}
-                    type={"text"}
-                    className="mt-2"
-                  />
-                </div>
-
-                <div className="mt-4 basis-1/2">
-                  <FormInput
-                    label={"Palabras clave"}
-                    control={control}
-                    name="palabrasClave"
-                    placeholder={"Ingrese las palabras claves"}
-                    type={"text"}
-                    className="mt-2"
-                  />
-                </div>
+              <div className="mt-4 basis-1/3">
+                <SelectMarcasForm
+                  name="marca"
+                  realNameId="marcaId"
+                  control={control}
+                  className="mt-2"
+                  label={"Marca"}
+                  placeholder={"Seleccione una marca"}
+                />
               </div>
 
-              <div className="flex w-full flex-col gap-x-4 md:flex-row lg:flex-row lg:justify-between">
-                <div className="mt-4 basis-1/2">
-                  <SelectTipoForm
-                    name="tipo"
-                    realNameId="tipoId"
-                    control={control}
-                    className="mt-2"
-                    label={"Tipo"}
-                    placeholder={"Seleccione un tipo"}
-                  />
-                </div>
+              <div className="mt-4 basis-1/3">
+                <SelectModelosForm
+                  label={"Modelo"}
+                  control={control}
+                  name="modeloForm"
+                  realNameId="modelo"
+                  className="mt-2"
+                  placeholder={"Seleccione un modelo"}
+                />
+              </div>
+            </div>
 
-                <div className="mt-4 basis-1/2">
-                  <SelectEstadoForm
-                    name="estadoId"
-                    control={control}
-                    className="mt-2"
-                    label={"Estado"}
-                    placeholder={"Seleccione un estado"}
-                  />
-                </div>
+            <div className="flex w-full flex-col gap-x-4 md:flex-row lg:flex-row lg:justify-between">
+              <div className="mt-4 basis-1/2">
+                <FormInput
+                  label={"Número de serie"}
+                  control={control}
+                  name="numeroSerie"
+                  placeholder={"Ingrese el número de serie"}
+                  type={"text"}
+                  className="mt-2"
+                />
               </div>
 
-              <div className="flex w-full flex-col gap-x-4 md:flex-row lg:flex-row lg:justify-between">
-                <div className="mt-4 basis-1/2">
-                  <SelectSedeForm
-                    name="sedeId"
-                    control={control}
-                    className="mt-2"
-                    label={"Sede"}
-                    placeholder={"Seleccione una sede"}
-                    onChange={() => {
-                      // @ts-expect-error - undefined
-                      formHook.setValue("laboratorioId", undefined);
-                      formHook.setValue("armarioId", undefined);
-                      formHook.setValue("estanteId", undefined);
-                    }}
-                  />
-                </div>
+              <div className="mt-4 basis-1/2">
+                <FormInput
+                  label={"Palabras clave"}
+                  control={control}
+                  name="palabrasClave"
+                  placeholder={"Ingrese las palabras claves"}
+                  type={"text"}
+                  className="mt-2"
+                />
+              </div>
+            </div>
 
-                <div className="mt-4 basis-1/2">
-                  <SelectLaboratorioConArmariosForm
-                    name="laboratorioId"
-                    control={control}
-                    className="mt-2"
-                    label={"Laboratorio"}
-                    sedeId={sedeId ? String(sedeId) : undefined}
-                    disabled={!sedeId}
-                    placeholder={!sedeId ? "Seleccione una sede" : "Seleccione un laboratorio"}
-                    onChange={() => {
-                      formHook.setValue("armarioId", undefined);
-                      formHook.setValue("estanteId", undefined);
-                    }}
-                  />
-                </div>
+            <div className="flex w-full flex-col gap-x-4 md:flex-row lg:flex-row lg:justify-between">
+              <div className="mt-4 basis-1/2">
+                <SelectTipoForm
+                  name="tipo"
+                  realNameId="tipoId"
+                  control={control}
+                  className="mt-2"
+                  label={"Tipo"}
+                  placeholder={"Seleccione un tipo"}
+                />
               </div>
 
-              <div className="flex w-full flex-col gap-x-4 md:flex-row lg:flex-row lg:justify-between">
-                <div className="mt-4 basis-1/2">
-                  <SelectArmarioForm
-                    name="armarioId"
-                    control={control}
-                    className="mt-2"
-                    label={"Armario"}
-                    laboratorioId={laboratorioId}
-                    placeholder={!laboratorioId ? "Seleccione un laboratorio" : "Seleccione un armario"}
-                    onChange={() => {
-                      formHook.setValue("estanteId", undefined);
-                    }}
-                  />
-                </div>
+              <div className="mt-4 basis-1/2">
+                <SelectEstadoForm
+                  name="estadoId"
+                  control={control}
+                  className="mt-2"
+                  label={"Estado"}
+                  placeholder={"Seleccione un estado"}
+                />
+              </div>
+            </div>
 
-                <div className="mt-4 basis-1/2">
-                  <SelectEstanteForm
-                    name="estanteId"
-                    control={control}
-                    className="mt-2"
-                    label={"Estante"}
-                    armarioId={armarioId}
-                    placeholder={!armarioId ? "Seleccione un armario" : "Seleccione un estante"}
-                  />
-                </div>
+            <div className="flex w-full flex-col gap-x-4 md:flex-row lg:flex-row lg:justify-between">
+              <div className="mt-4 basis-1/2">
+                <SelectSedeForm
+                  name="sedeId"
+                  control={control}
+                  className="mt-2"
+                  label={"Sede"}
+                  placeholder={"Seleccione una sede"}
+                  onChange={() => {
+                    // @ts-expect-error - undefined
+                    formHook.setValue("laboratorioId", undefined);
+                    formHook.setValue("armarioId", undefined);
+                    formHook.setValue("estanteId", undefined);
+                  }}
+                />
               </div>
 
-              <div className="flex w-full flex-col gap-x-4 md:flex-row lg:flex-row lg:justify-between">
-                <div className="mb-1 mt-4 h-auto w-full">
-                  <FormTextarea label={"Observaciones"} name="observaciones" control={control} id="observaciones" />
-                </div>
+              <div className="mt-4 basis-1/2">
+                <SelectLaboratorioConArmariosForm
+                  name="laboratorioId"
+                  control={control}
+                  className="mt-2"
+                  label={"Laboratorio"}
+                  sedeId={sedeId ? String(sedeId) : undefined}
+                  disabled={!sedeId}
+                  placeholder={!sedeId ? "Seleccione una sede" : "Seleccione un laboratorio"}
+                  onChange={() => {
+                    formHook.setValue("armarioId", undefined);
+                    formHook.setValue("estanteId", undefined);
+                  }}
+                />
+              </div>
+            </div>
+
+            <div className="flex w-full flex-col gap-x-4 md:flex-row lg:flex-row lg:justify-between">
+              <div className="mt-4 basis-1/2">
+                <SelectArmarioForm
+                  name="armarioId"
+                  control={control}
+                  className="mt-2"
+                  label={"Armario"}
+                  laboratorioId={laboratorioId}
+                  placeholder={!laboratorioId ? "Seleccione un laboratorio" : "Seleccione un armario"}
+                  onChange={() => {
+                    formHook.setValue("estanteId", undefined);
+                  }}
+                />
+              </div>
+
+              <div className="mt-4 basis-1/2">
+                <SelectEstanteForm
+                  name="estanteId"
+                  control={control}
+                  className="mt-2"
+                  label={"Estante"}
+                  armarioId={armarioId}
+                  placeholder={!armarioId ? "Seleccione un armario" : "Seleccione un estante"}
+                />
+              </div>
+            </div>
+
+            <div className="flex w-full flex-col gap-x-4 md:flex-row lg:flex-row lg:justify-between">
+              <div className="mb-1 mt-4 h-auto w-full">
+                <FormTextarea label={"Observaciones"} name="observaciones" control={control} id="observaciones" />
               </div>
             </div>
           </div>
-        </ScrollArea>
+        </div>
 
         <div className="mb-3 flex w-full flex-row items-end justify-center space-x-4 md:justify-end">
           <Button title="Cancelar" type="button" variant="default" color="secondary" onClick={handleCancel}>
