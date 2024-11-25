27073d348bb50f3faada20e6914baed985de31f3
santiagocast <santiagojcastelli@gmail.com>
2024-11-25 02:54:54 -0300

    fix trpc circle dependencies

---
+++ src/server/api/services/permisos/permisos.helper.ts
@@ -0,0 +1,12 @@
+import { type PrismaClient, type SgeNombre } from "@prisma/client";
+import { verificarPermisoUsuario } from "../../repositories/permisos/permisos.repository";
+
+export const tienePermiso = async (ctx: { db: PrismaClient }, permisos: SgeNombre[], userId: string) => {
+  if (permisos.length === 0) {
+    return true;
+  }
+
+  const tienePermiso = await verificarPermisoUsuario(ctx, userId, permisos);
+
+  return tienePermiso;
+};
