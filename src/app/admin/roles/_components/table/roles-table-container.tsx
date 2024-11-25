c0a3b568c48c24b4254c9027b9386247b6b2c4d1
Alexander Armua <alexanderarmua1@gmail.com>
2024-11-25 18:42:20 -0300

    fix loading

--- src/app/admin/roles/_components/table/roles-table-container.tsx
+++ src/app/admin/roles/_components/table/roles-table-container.tsx
@@ -5,6 +5,7 @@ import { type z } from "zod";
 import { type inputGetRoles } from "@/shared/filters/admin-roles-filter.schema";
 import { RolesTable } from "./roles-table";
 import LoadingAdminTable from "./loading-admin-table";
+import { adminRolesColumnas } from "./columns";
 
 type RolesFilters = z.infer<typeof inputGetRoles>;
 
@@ -12,12 +13,11 @@ type LaboratoriosTableContainerProps = {
   filters: RolesFilters;
 };
 
-// TODO @Alex: Va a ser mejor convertir todos estos componentes de tablas en `use client` porque asi TRPC podrá invalidar los hooks y hacer queries desde los modals
 export default function RolesTableContainer({ filters }: LaboratoriosTableContainerProps) {
   const { data: roles, isLoading } = api.admin.roles.getAllRoles.useQuery(filters);
 
   if (isLoading) {
-    return LoadingAdminTable;
+    return <LoadingAdminTable columns={adminRolesColumnas} />;
   }
 
   return <RolesTable data={roles ?? { count: 0, roles: [] }} filters={filters} />;
