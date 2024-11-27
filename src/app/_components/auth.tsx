513691117eef1bca7c7666c1e2848788d625cb64
Agustina Razanov <agustinarazanov@gmail.com>
2024-11-27 20:08:31 -0300

    next auth pages

--- src/app/_components/auth.tsx
+++ src/app/_components/auth.tsx
@@ -1,21 +1,14 @@
 "use client";
 
-import { signIn, signOut, type ClientSafeProvider } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
 
 const style = "rounded-full bg-slate-100 px-10 py-3 font-semibold no-underline transition hover:bg-slate-200";
 
-export function SignIn(providers: ClientSafeProvider[]) {
+export function SignIn() {
   return (
-    <>
-      {providers &&
-        Object.values(providers)
-          .filter((provider) => provider.id === "keycloak")
-          .map((provider) => (
-            <button key={provider.id} className={style} onClick={() => signIn(provider.id)}>
-              Iniciar sesión
-            </button>
-          ))}
-    </>
+    <button className={style} onClick={() => signIn("discord")}>
+      Iniciar sesión
+    </button>
   );
 }
 
