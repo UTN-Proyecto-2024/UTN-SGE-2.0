a064f51c67ad00875c32064c436312d7bf9907ff
Alex <alexanderarmua1@gmail.com>
2024-11-26 23:51:00 -0300

    fixes generales

--- src/app/equipos/tipos/@modal/(...)equipos/tipos/[id]/page.tsx
+++ src/app/equipos/tipos/@modal/(...)equipos/tipos/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import ModalDrawer from "@/app/_components/modal/modal-drawer";
 import { TipoForm } from "@/app/equipos/tipos/[id]/tipo-form";
+import { ScrollArea } from "@/components/ui";
 import { api } from "@/trpc/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -42,10 +43,11 @@ export default function PageDetails({ params: { id } }: PageProps) {
       open={open}
       onOpenChange={handleOpenChange}
       trigger={<></>}
-      className={"max-h-[calc(100vh_-_300px)]"}
     >
       <div className="flex max-h-max w-full flex-col  gap-4">
-        <TipoForm id={id} onCancel={handleClickCancel} onSubmit={handleClickSave} />
+        <ScrollArea className="max-h-[calc(100vh_-_300px)] w-full pr-4">
+          <TipoForm id={id} onCancel={handleClickCancel} onSubmit={handleClickSave} />
+        </ScrollArea>
       </div>
     </ModalDrawer>
   );
--- src/app/laboratorios/solicitudes/[id]/form-gestion-reserva.tsx
+++ src/app/laboratorios/solicitudes/[id]/form-gestion-reserva.tsx
@@ -42,41 +42,31 @@ export const ReservaViewAdmin = ({ reservaId, onCancel, onAprobar, onRechazar }:
     esReservaPasada && !estaCancelada && reservaData?.reserva.estatus === ReservaEstatus.PENDIENTE;
 
   return (
-    <ScrollArea className="max-h-[calc(100vh_-_300px)]">
-      <div className="container mx-auto space-y-8 p-4">
-        {esReservaPendientePasada && (
-          <Alert variant={"destructive"} className="w-full bg-red-500 text-white">
-            <AlertTitle className="flex flex-row items-center">
-              <ClockIcon className="mr-2 h-4 w-4" />
-              Reserva pendiente pasada
-            </AlertTitle>
-            <AlertDescription>
-              <p className="text-sm">
-                <b>
-                  Esta reserva quedo en el estado pendiente y se encuentra en el pasado, solo es posible cancelarla.
-                </b>
-              </p>
-            </AlertDescription>
-          </Alert>
-        )}
-        <ReservaDetalle reservaId={reservaId} mostrarCompleto={true} />
-        {!esReservaPasada && !estaCancelada && (
-          <ReservaAprobacion reservaId={reservaId} onCancel={onCancel} onAprobar={onAprobar} onRechazar={onRechazar} />
-        )}
-        {esReservaPendientePasada && (
-          <div className="flex justify-end">
-            <Button
-              title="Cancelar Reserva"
-              type="button"
-              variant="default"
-              color="danger"
-              onClick={handleCancelReserva}
-            >
-              Cancelar Reserva
-            </Button>
-          </div>
-        )}
-      </div>
-    </ScrollArea>
+    <div className="container mx-auto space-y-8 p-4">
+      {esReservaPendientePasada && (
+        <Alert variant={"destructive"} className="w-full bg-red-500 text-white">
+          <AlertTitle className="flex flex-row items-center">
+            <ClockIcon className="mr-2 h-4 w-4" />
+            Reserva pendiente pasada
+          </AlertTitle>
+          <AlertDescription>
+            <p className="text-sm">
+              <b>Esta reserva quedo en el estado pendiente y se encuentra en el pasado, solo es posible cancelarla.</b>
+            </p>
+          </AlertDescription>
+        </Alert>
+      )}
+      <ReservaDetalle reservaId={reservaId} mostrarCompleto={true} />
+      {!esReservaPasada && !estaCancelada && (
+        <ReservaAprobacion reservaId={reservaId} onCancel={onCancel} onAprobar={onAprobar} onRechazar={onRechazar} />
+      )}
+      {esReservaPendientePasada && (
+        <div className="flex justify-end">
+          <Button title="Cancelar Reserva" type="button" variant="default" color="danger" onClick={handleCancelReserva}>
+            Cancelar Reserva
+          </Button>
+        </div>
+      )}
+    </div>
   );
 };
