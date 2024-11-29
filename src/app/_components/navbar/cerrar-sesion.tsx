eeb9afedc285ab0cd568f3932bcf63ada83d6d45
Alexander Armua <alexanderarmua1@gmail.com>
2024-11-29 02:53:54 -0300

    Agregado logout

---
+++ src/app/_components/navbar/cerrar-sesion.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { MenuItem } from "@headlessui/react";
+import { signOut } from "next-auth/react";
+
+export const ProfileMenuCerrarSesion = () => (
+  <MenuItem>
+    <button
+      type="button"
+      className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-[focus]:bg-slate-100"
+      onClick={() => signOut()}
+    >
+      Cerrar sesión
+    </button>
+  </MenuItem>
+);
