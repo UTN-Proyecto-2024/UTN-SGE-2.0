38efc62060af0d35d4f77b4ebce0fea0c3bdbbcf
Agustina Razanov <agustina@Agustina.local>
2025-02-13 12:37:57 -0300

    fix key not a prop warning

--- src/app/_components/navbar/profile-menu-item.tsx
+++ src/app/_components/navbar/profile-menu-item.tsx
@@ -1,7 +1,7 @@
 import { MenuItem } from "@headlessui/react";
 
-export const ProfileMenuItem = ({ key, href, label }: { key: string; href: string; label: string }) => (
-  <MenuItem key={key}>
+export const ProfileMenuItem = ({ href, label }: { href: string; label: string }) => (
+  <MenuItem>
     <a href={href} className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-slate-100">
       {label}
     </a>
